68e97cebb1813eba06934bebddfc19715b5a8ad9
PHILIPPE MULET <philippe_mulet@fr.ibm.com>
2016-07-05 07:13:27 -0500

    register microservice

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
+/*globals cloudantService:true */
+/*eslint-env node, express*/
 var express = require('express');
 var bodyParser = require('body-parser');
 var cfenv = require("cfenv");
 var path = require('path');
 var cors = require('cors');
+var http = require('http');
 
 //Setup Cloudant Service.
 var appEnv = cfenv.getAppEnv();
 cloudantService = appEnv.getService("myMicroservicesCloudant");
 var items = require('./routes/items');
 
-//Setup middleware.
+console.log("vcap:", appEnv);
+
+//Setup Service Discovery
+var serviceDiscovery = appEnv.getService("myMicroserviceDiscovery");
+var options = {
+  "host": serviceDiscovery.credentials.url,
+  "port": 443,
+  "path": "/api/v1/instances",
+  "postData": "{ 'tags' :[] , 'status' :'UP' , 'service_name' :'catalog-api' , 'ttl' :'300' , 'endpoint' : { 'value' :'xxx.xxx.xxx.xx' , 'type' :'http' } }",
+  method: "POST"
+};
+http.request(options, function(res) {
+  console.log("STATUS: " + res.statusCode);
+  console.log("HEADERS: " + JSON.stringify(res.headers));
+  res.setEncoding("utf8");
+  res.on("data", function (chunk) {
+    console.log("BODY: " + chunk);
+  });
+}).end();
+
+//Setup Middleware.
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -29,3 +52,5 @@ app.delete('/items/:id', items.remove);
 
 app.listen(appEnv.port, appEnv.bind);
 console.log('App started on ' + appEnv.bind + ':' + appEnv.port);
+
+
