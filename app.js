014023a282721d795c5653a09bc698aa651bc844
PHILIPPE MULET <philippe_mulet@fr.ibm.com>
2016-07-08 06:43:47 -0500

    ab

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-/*globals discoveryService:true cloudantService:true*/
+/*globals discovery:true cloudantService:true*/
 /*eslint-env node, express*/
 var express = require('express');
 var bodyParser = require('body-parser');
 var cfenv = require("cfenv");
 var path = require('path');
 var cors = require('cors');
-var discovery = require('bluemix-service-discovery');
-var http = require('http');
+var ServiceDiscovery = require('bluemix-service-discovery');
 
 //Setup Cloudant Service.
 var appEnv = cfenv.getAppEnv();
@@ -17,10 +16,48 @@ console.log("###VCAP: " + JSON.stringify(appEnv));
 
 //Setup Service Discovery
 var sdcreds = appEnv.getService("myMicroservicesDiscovery").credentials;
+discovery = new ServiceDiscovery({
+  name: 'ServiceDiscovery',
+  auth_token: sdcreds.auth_token,
+  url: sdcreds.url,
+  version: 1
+});
 
-console.log("###SD auth token: " + sdcreds.auth_token);
-console.log("###SD url: " + sdcreds.url);
+var service_instance = {
+  service_name: 'catalog-api',
+  ttl: 30, // 30s
+  endpoint: {
+    type: 'http',
+    value: appEnv.url
+  },
+  metadata: {
+//    foo: 'bar'
+  }
+};
 
+// Register and then send a heartbeat (renew)
+discovery.register(service_instance, function(err, response, body) {
+  if (!err && response.statusCode === 201) {
+    var id = body.id;
+    console.log('Registered', body);
+    setInterval(function() {
+      discovery.renew(id, function(err, response) {
+        if (!err && response.statusCode === 200) {
+          console.log('HEARTBEAT OK');
+        } else {
+          console.log('HEARTBEAT ERROR');
+        }
+      });
+    }, 3000);
+  }
+});
+
+
+
+/*
+
+
+//----------------------
 discoveryService = new discovery({
   name: 'ServiceDiscovery',
   auth_token: sdcreds.auth_token,
@@ -67,7 +104,7 @@ http.request(options, function(res) {
   res.on("data", function (chunk) {
     console.log("BODY: " + chunk);
   });
-}).end();
+}).end();*/
 
 
 //Setup Middleware.
